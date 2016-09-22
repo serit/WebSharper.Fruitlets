d1b93b956aff8c620c9cb6e0f6d6fc56a6f6767a
Klaske Van Vuurden <klaske@itpartner.no>
2016-09-22 15:26:18 +0200

    Module window type (v0.0.2)

--- WebSharper.UI.Next.Serit/Modal.fs
+++ WebSharper.UI.Next.Serit/Modal.fs
@@ -11,6 +11,63 @@ module Modal =
     let private divClass ``class`` content =
         divAttr [attr.``class`` ``class``] content
 
+    type WindowSize =
+        | Small
+        | Normal
+        | Large
+        | Custom of int * int
+    and Window =
+        {
+            Id: string
+            Header: Doc
+            Body: Doc
+            Footer: Doc
+            Size: WindowSize
+        }
+        member this.Show () =
+
+            let dialogSizeAttrs =
+                match this.Size with
+                | Large -> [attr.``class`` "modal-dialog modal-lg"]
+                | _ -> [attr.``class`` "modal-dialog"]
+
+            divAttr[
+                attr.``class`` "modal fade"
+                attr.id this.Id
+                attr.tabindex "-1"
+                Attr.Create "role" "dialog"
+                attr.style "overflow:scroll"
+                ][
+                divAttr (
+                    [
+                     Attr.Create "role" "document"
+                    ] @ dialogSizeAttrs )[
+                        divClass "modal-content fs-modal-content"
+                            [
+                                this.Header
+                                divClass "modal-body" [this.Body]
+                                this.Footer
+                            ]
+                    ]
+            ]
+        static member Create Id header body footer size =
+            {
+                Id = Id
+                Header = header
+                Body = body
+                Footer = footer
+                Size = size
+            }
+        static member empty =
+            {
+                Id = "modal"
+                Header = Doc.Empty
+                Body = Doc.Empty
+                Footer = Doc.Empty
+                Size = WindowSize.Normal
+            }
+
+
     let Button Id attrList title =
         buttonAttr (
             [
@@ -30,26 +87,5 @@ module Modal =
                 attr.``data-`` "dismiss" "modal"
             ][text "x"]
             h4[text title]
-        ]
-
-    let Window modalId size header body footer =
-        divAttr[
-            attr.``class`` "modal fade"
-            attr.id modalId
-            attr.tabindex "-1"
-            Attr.Create "role" "dialog"
-            attr.style "overflow:scroll"
-            ][
-            divAttr[
-                attr.``class`` <| "modal-dialog" + size
-                Attr.Create "role" "document"
-                ][
-                    divClass "modal-content fs-modal-content"
-                        [
-                            header
-                            divClass "modal-body" [body]
-                            footer
-                        ]
-                ]
         ]
 
