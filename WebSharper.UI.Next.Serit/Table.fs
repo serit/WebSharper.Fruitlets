3165152a74ebb751f4a48508a18f595fa148e540
Klaske Van Vuurden <klaske@itpartner.no>
2016-09-23 13:29:48 +0200

    Table column content more flexible, with easier creation

--- WebSharper.UI.Next.Serit/Table.fs
+++ WebSharper.UI.Next.Serit/Table.fs
@@ -28,22 +28,22 @@ module Table =
             | _ -> ""
 
     [<StructuralComparison;StructuralEquality>]
-    type ColumnType =
+    type SortableType =
         | I of int
         | S of string
         | F of float
         | D of System.DateTime
         static member DefaultShow =
-            fun (ct : ColumnType) ->
-                match ct with
+            fun (st : SortableType) ->
+                match st with
                 | I i -> sprintf "%i" i
                 | S s -> s
                 | F f -> sprintf "%f" f
                 | D d -> d.ToShortDateString()
         //| T of obj //* ('T -> ColumnType<'T>)
 
-    type ColumnShow =
-        (ColumnType -> string)
+    type SortShow =
+        (SortableType -> string)
 
     type TablePages =
         {
@@ -53,53 +53,73 @@ module Table =
     type TableClass =
         | Striped
         | Bordered
+        | Custom of string
         member this.show =
             match this with
             | Striped -> "table-striped"
             | Bordered -> "table-bordered"
+            | Custom cl -> cl
 
-    type Table<'U,'T> when 'U : equality =
+    type Column<'T> =
+        | Sortable of string * ('T -> list<Attr>) * ('T -> list<Doc>) * ('T -> SortableType)
+        | Plain of string * ('T -> list<Attr>) * ('T -> list<Doc>)
+        member this.showHeader index table =
+            match this with
+            | Sortable (name, _, _, sortFunc) ->
+
+                tdAttr([
+                        on.click ( fun _ _ ->
+                            table.Direction <- table.Direction.Flip index
+
+                            (sortFunc, table.RowData.Value)
+                            ||> table.Direction.SortFunc index
+                            |> table.RowData.Set
+                        )
+                ] )[
+                    text name
+                    iAttr[
+                        attr.style "color: #aaa;"
+                        attr.``class`` <| table.Direction.FAClass index
+                    ][] :> Doc
+                ] :> Doc
+            | Plain (name, _,_ ) ->
+                td [
+                    text name
+                ] :> Doc
+
+        member this.showRow item =
+            match this with
+            | Sortable (_, attrList, docList, _) ->
+                tdAttr ( attrList item ) (docList item)
+            | Plain (_, attrList, docList) ->
+                tdAttr ( attrList item ) (docList item)
+        static member SimpleColumn (name, fieldValueFunction : ('T -> int)) =
+            Plain (name, (fun t -> []), (fun t -> [text << string <| fieldValueFunction t ]))
+        static member SimpleColumn (name, fieldValueFunction : ('T -> string)) =
+            Plain (name, (fun t -> []), (fun t -> [text <| fieldValueFunction t ]))
+        static member SimpleColumn (name, fieldValueFunction : ('T -> float)) =
+            Plain (name, (fun t -> []), (fun t -> [text << string <| fieldValueFunction t ]))
+        static member SimpleSortColumn (name, fieldValueFunction : ('T -> int)) =
+            Sortable (name, (fun t -> []), (fun t -> [text << string <| fieldValueFunction t ]), (fun t -> I <| fieldValueFunction t ))
+        static member SimpleSortColumn (name, fieldValueFunction : ('T -> string)) =
+            Sortable (name, (fun t -> []), (fun t -> [text <| fieldValueFunction t ]), (fun t -> S <| fieldValueFunction t ))
+        static member SimpleSortColumn (name, fieldValueFunction : ('T -> float)) =
+            Sortable (name, (fun t -> []), (fun t -> [text << string <| fieldValueFunction t ]), (fun t -> F <| fieldValueFunction t ))
+
+    and Table<'U,'T> when 'U : equality =
         {
             Id: string
             Class: TableClass []
             Paging: TablePages option
-            Sortable: bool
             RowData: ListModel<'U, 'T>
-            Columns: (string * ('T -> ColumnType) * ColumnShow) []
+            Columns: Column<'T> []
             mutable Direction: SortDirection
         }
         member this.ShowHeader () =
             this.Columns
             |> Array.indexed
-            |> Array.map (fun (index, (name, value, show)) ->
-                tdAttr[
-                    (if this.Sortable
-                    then
-                        on.click ( fun _ _ ->
-                            let convertFunction (t : 'T) =
-                                t |> value |> show
-                            this.Direction <- this.Direction.Flip index
-
-                            (convertFunction, this.RowData.Value)
-                            ||> this.Direction.SortFunc index
-                            |> this.RowData.Set
-                        )
-                    else
-                        Attr.Empty
-                    )
-                ][
-                    text name
-                    (
-                        if this.Sortable
-                        then
-                            iAttr[
-                                attr.style "color: #aaa;"
-                                attr.``class`` <| this.Direction.FAClass index
-                            ][] :> Doc
-                        else
-                            Doc.Empty
-                    )
-                ] :> Doc
+            |> Array.map (fun (index, column) ->
+                column.showHeader index this
             )
             |> fun headerRow -> thead[tr headerRow] :> Doc
         member this.ShowTable () =
@@ -108,8 +128,8 @@ module Table =
                 |> Seq.map ( fun t ->
                     let row =
                         this.Columns
-                        |> Array.map (fun (name, value, show) ->
-                            td [t |> value |> show |> text] :> Doc
+                        |> Array.map (fun column ->
+                            column.showRow t :> Doc
                         )
                     tr row :> Doc
                 )
@@ -126,6 +146,6 @@ module Table =
 
             Doc.BindView ( fun view ->
                 tableAttr [attr.``class`` classes ]
-                    (this.ShowHeader() :: rows view)
+                    (this.ShowHeader() :: [tbody <| rows view])
             ) this.RowData.View
 
