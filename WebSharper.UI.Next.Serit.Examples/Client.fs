3f2eb2aab53d91c332df12948d6f5e2d959c6738
Klaske Van Vuurden <klaske@itpartner.no>
2016-09-23 13:30:11 +0200

    Adjusted example to new table creation

--- WebSharper.UI.Next.Serit.Examples/Client.fs
+++ WebSharper.UI.Next.Serit.Examples/Client.fs
@@ -3,8 +3,12 @@ namespace WebSharper.UI.Next.Serit.Examples
 open WebSharper
 open WebSharper.JavaScript
 open WebSharper.UI.Next
+open WebSharper.UI.Next.Html
 open WebSharper.UI.Next.Client
 open WebSharper.UI.Next.Serit
+
+//open FSharp.Data
+
 
 [<JavaScript>]
 module Client =
@@ -13,20 +17,23 @@ module Client =
 
     type IndexTemplate = Templating.Template<"index.html">
 
-    let data = [1..10]
 
-
+    let dataList = ['a'..'z'] |> List.indexed
 
     let tbl =
         {
             Id = "table"
-            Sortable = true
-            RowData = ListModel.Create id data
-            Class = [| Striped |]
+            RowData = ListModel.Create id dataList
+            Class = [| Striped ; Bordered |]
             Columns =
                 [|
-                    "Id", I << id, ColumnType.DefaultShow
-                    "Square", (fun x -> I <| x*x), ColumnType.DefaultShow
+                        Column.SimpleColumn ("Id", (fun ((i:int),_) -> i))
+                        Column.SimpleColumn ("Square", (fun ((i:int),_) -> i * i))
+                        Column.SimpleColumn ("To Char", (fun (_,c) -> string c |> Seq.replicate 5 |> Seq.toArray |> String.concat ""))
+                        Column.SimpleSortColumn ("Id", (fun ((i:int),_) -> i))
+                        Column.SimpleSortColumn ("Square", (fun ((i:int),_) -> i * i))
+                        Column.SimpleSortColumn ("To Char", (fun (_,c) -> string c |> Seq.replicate 5 |> Seq.toArray |> String.concat ""))
+
                 |]
             Direction = Asc 1
             Paging = None
