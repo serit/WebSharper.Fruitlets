8bf943f41401c3e654591a36eff0b3d2ff11915f
Klaske Van Vuurden <klaske@itpartner.no>
2016-09-26 23:38:05 +0200

    Extended example

--- WebSharper.UI.Next.Serit.Examples/Client.fs
+++ WebSharper.UI.Next.Serit.Examples/Client.fs
@@ -6,43 +6,102 @@ open WebSharper.UI.Next
 open WebSharper.UI.Next.Html
 open WebSharper.UI.Next.Client
 open WebSharper.UI.Next.Serit
+open WebSharper.UI.Next.Server
+open WebSharper.Sitelets
 
-//open FSharp.Data
+open FSharp.Data
 
 
 [<JavaScript>]
 module Client =
 
+    [<Inline "$obj[$field]">]
+    let GetField field obj = X<obj>
+
     open WebSharper.UI.Next.Serit.Table
+
+    let Body () =
+        let gameList = ListModel.Create (fun (r: Server.GameObject) -> r.Title) Array.empty
+
+        async {
+            let! gl = Server.GetGames()
+            gameList.Set gl
+        } |> Async.Start
+
+        let inputTitleLens (g: Server.GameObject) =
+            let v = Var.Create g.Title
+            [Doc.Input [on.change (fun el ev -> gameList.UpdateBy ( fun g -> Some {g with Title = v.Value}) g.Title)] v :> Doc]
+
+
+        let columns =
+//            Server.GetGameNames()
+//            |> Array.map (fun name ->
+//                let mapfunc = (fun (g : Server.GameObject) ->
+//                    let result = GetField name g
+//                    As result
+//                    match result with
+//                    | :? int as i -> Column.SimpleColumn (name, (fun g -> i))
+//                    | :? float as f -> Column.SimpleColumn (name, (fun g -> f))
+//                    | :? string as s -> Column.SimpleColumn (name, (fun g -> s))
+//                    | _ -> Column.SimpleColumn (name, (fun g -> string result))
+//                    )
+//                mapfunc
+//            )
+//        Console.Log columns
+            [|
+                Column.SimpleSortColumn ("Title" , (fun (r: Server.GameObject) -> r.Title))
+                Column.Plain ("Editable title", (fun g -> []), (fun g -> inputTitleLens g ))
+                Column.Editable ("Editable title special", (fun g -> []), (fun g -> inputTitleLens g ), None, (fun g -> StringInput ((fun g -> g.Title),(fun g t -> {g with Title = t}))))
+                Column.SimpleSortColumn ("Rating" , (fun (r: Server.GameObject) -> r.Rating))
+                Column.SimpleSortColumn ("Voters" , (fun (r: Server.GameObject) -> r.Voters))
+                Column.SimpleSortColumn ("Rank" , (fun (r: Server.GameObject) -> r.Rank))
+                //Column.Plain ("", (fun g -> []), (fun g -> [buttonAttr[on.click (fun el ev -> Console.Log g)] [text "Save"]] ))
+            |]
+
+        let gameTable =
+            {
+                Id = "gameTable"
+                RowData = gameList
+                RowIdFunc = (fun r -> r.Title)
+                Class = [| Striped ; Bordered |]
+                Columns = columns
+                Direction = Asc 3
+            }
+
+//        let reflectedgames = Var.Create None
+//        async {
+//             let! rg = Server.ReflectedGames()
+//             reflectedgames.Value <-
+//                let tbl = Table<string,Server.Game.Row>.Create rg
+//                Some tbl
+//        } |> Async.Start
+
+        let newName = Var.Create ""
+        div [
+              h2 [text "normal"]
+              gameTable.ShowTableWithPages 10
+              h2 [text "reflected"]
+//              Doc.BindView( fun (v : option<Table<_,_>>) ->
+//                    match v with
+//                    | Some tbl -> tbl.ShowTableWithPages 10
+//                    | None -> Doc.Empty
+//                ) reflectedgames.View
+        ]
+
+type Endpoint =
+    |[<EndPoint "/">] Home
+
+module Site =
 
     type IndexTemplate = Templating.Template<"index.html">
 
-
-    let dataList = ['a'..'z'] |> List.indexed
-
-    let tbl =
-        {
-            Id = "table"
-            RowData = ListModel.Create (fun (i,c) -> i ) dataList
-            RowIdFunc = (fun (i,c) -> i)
-            Class = [| Striped ; Bordered |]
-            Columns =
-                [|
-                        Column.SimpleColumn ("Id", (fun ((i:int),_) -> i))
-                        Column.SimpleColumn ("Square", (fun ((i:int),_) -> i * i))
-                        Column.SimpleColumn ("To Char", (fun (_,c) -> string c |> Seq.replicate 5 |> Seq.toArray |> String.concat ""))
-                        Column.SimpleSortColumn ("Id", (fun ((i:int),_) -> i))
-                        Column.SimpleSortColumn ("Square", (fun ((i:int),_) -> i * i))
-                        Column.SimpleSortColumn ("To Char", (fun (_,c) -> string c |> Seq.replicate 5 |> Seq.toArray |> String.concat ""))
-
-                |]
-            Direction = Asc 1
-        }
-
+    [<Website>]
     let Main =
-        let newName = Var.Create ""
 
-        IndexTemplate.Main.Doc(
-            body = [tbl.ShowTableWithPages 5]
-        )
-        |> Doc.RunById "main"
+        Sitelet.Infer <| fun ctx action ->
+            match action with
+            | Home ->
+                Content.Page(
+                    IndexTemplate.Doc(
+                        body = [div [ client <@ Client.Body() @>]]
+                     ))
---
+++ WebSharper.UI.Next.Serit.Examples/Remoting.fs
@@ -0,0 +1,88 @@
+namespace WebSharper.UI.Next.Serit.Examples
+
+open WebSharper
+open WebSharper.UI.Next
+open WebSharper.UI.Next.Client
+open WebSharper.UI.Next.Html
+open WebSharper.Web
+
+open FSharp.Data
+open FSharp.Quotations
+//open Microsoft.FSharp. FSharp.Quotations.Typed
+
+open System.Reflection
+
+module Server =
+
+    open WebSharper.UI.Next.Serit.Table
+
+    type Fields =
+        | TypeFields of System.Type []
+        | PropertyFields of PropertyInfo []
+
+    let retrieve ( T : System.Type) =
+//        try
+            T.GetProperties()
+//        with
+//        | ex -> failwith ex.Message
+
+    let TableConstructor (data: seq<'T>) (T: System.Type) (idFunc: 'T -> 'U) =
+//        try
+            let Id = (typeof<'T>).Name
+            let fields =
+                retrieve T
+            let columns =
+                fields
+                |> Array.map ( fun pi ->
+                    let meth : ('T * array<_> -> obj) = pi.GetMethod.Invoke
+                    Column.SimpleColumn (pi.Name , (fun t -> meth(t, Array.empty)))
+                )
+            (Id, idFunc, columns, data)
+//        with
+//        | ex -> failwith ex.Message
+
+
+    [<Literal>]
+    let gamePath = "..\\data\\games.csv"
+
+    type Game =
+        CsvProvider<gamePath>
+
+    [<JavaScript>]
+    type GameObject =
+        { Title : string; Rating : float; Rank: int; Voters: int}
+        static member Create () =
+            { Title = ""; Rating = 0.0; Rank = 0; Voters = 0}
+        static member Create (title:string) =
+            { Title = title; Rating = 0.0; Rank = 0; Voters = 0}
+        static member Create (title:string, rating:float, rank:int,voters:int) =
+            { Title = title; Rating = rating; Rank = rank; Voters = voters}
+
+
+//    [<Rpc>]
+//    let ReflectedGames() =
+//
+//        async {
+//            let Games = Game.Load(gamePath)
+//            return TableConstructor Games.Rows (Games.Rows.GetType()) (fun g -> g.Title)
+//        }
+
+    [<Rpc>]
+    let GetGames () =
+        async {
+            let Games = Game.Load(gamePath)
+            return
+                Games.Rows
+                |> Seq.map (fun t -> GameObject.Create(t.Title,t.``Avg Rating`` |> float, t.``Board Game Rank``, t.``Num Voters``))
+                |> Seq.toArray
+        }
+
+    [<Rpc>]
+    let GetGameNames () =
+//        async {
+//            //let Games = Game.Load(gamePath)
+//            let columns =
+        retrieve typeof<GameObject>
+        |> Array.map ( fun pi -> pi.Name)
+//            return columns
+//        }
