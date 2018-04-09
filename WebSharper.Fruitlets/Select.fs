aa3d4f8cf7438f92c5ae945fbd0171a139a92ad7
Klaske Van Vuurden <klaske@itpartner.no>
2018-04-09 11:56:10 +0200

    Remove comments

--- WebSharper.Fruitlets/Select.fs
+++ WebSharper.Fruitlets/Select.fs
@@ -96,34 +96,13 @@ module Select =
                                 span[attr.``class`` "caret"][]
                             ]
                         ul
-                            (attrs @ [
-                                attr.``class`` "dropdown-menu"
-                                //on.change (fun el _ -> this.SelectedLens.Set << Some << this.TransformToKeyFromString <| getSelected el)
-//                                on.afterRender (fun el ->
-//                                    try
-//                                        // if an item is loaded and there exists a list, but the value selected is not the same as the target, then select something in the list and update the item accordingly  
-//                                        if selectedIndex el > -1 then
-//                                            match (this.SelectedOrigin.Value, getSelected el, List.tryHead mapList) with
-//                                            | Some _, selectedValue, Some (key, _) when this.TransformToKeyFromString selectedValue <> currentKey -> this.SelectedLens.Set <| Some key
-//                                            | _ -> ()
-//                                    with
-//                                        | exn -> Console.Log exn
-//                                    )
-                                ] )
+                            (attrs @ [ attr.``class`` "dropdown-menu" ] )
                             (
                                 mapList
                                 |> List.map ( fun (key, value) ->
                                     li
-                                        [
-                                            on.click (fun _ _ -> this.SelectedLens.Set <| Some key)
-                                            //attr.value <| this.TransformToStringFromKey key
-//                                            (if currentKey = key
-//                                            then attr.``class`` "selected"
-//                                            else
-//                                                Attr.Empty)
-                                        ] [
-                                            a[attr.href "javascript:false;"][value ()]
-                                            ] :> Doc
+                                        [ on.click (fun _ _ -> this.SelectedLens.Set <| Some key) ] 
+                                        [ a[attr.href "javascript:false;"][value ()] ] :> Doc
                                     )
                             ) :> Doc
                     ]
@@ -165,85 +144,3 @@ module Select =
                 
             }
         selectObject.Show attrs
-//
-//
-//    let private Select' attrs (options: Var<Map<int,string>>) (targetLens:Var<int option>) (datatype: Var<'T option>) =
-//        let detectChangeInOptionsAndItem = View.Map2 (fun opt target -> opt, target) options.View targetLens.View 
-//        Doc.BindView( fun ((map : Map<int,string>), (target: int option)) ->
-//            let mapList = Map.toList map
-//            let current =
-//                match target with
-//                | Some value -> value
-//                | _ -> -1
-//            selectAttr
-//                ([
-//                    attr.``class`` "fruit-form-select"
-//                    on.change (fun el _ -> targetLens.Set << Some << int <| getSelected el)
-//                    on.afterRender (fun el ->
-//                        try
-//                            // if an item is loaded and there exists a list, but the value selected is not the same as the target, then select something in the list and update the item accordingly  
-//                            if selectedIndex el > -1 then
-//                                match (datatype.Value, getSelected el, List.tryHead mapList) with
-//                                | Some _, selectedValue, Some (key, _) when int selectedValue <> current -> targetLens.Set <| Some key
-//                                | _ -> ()
-//                        with
-//                            | exn -> Console.Log exn
-//                        )
-//                    ] @ attrs )
-//                (
-//                    mapList
-//                    |> List.map ( fun (key, value) ->
-//                        Doc.Element
-//                            "option"
-//                            [
-//                                attr.``class`` "fruit-form-select-option"
-//                                attr.value <| string key
-//                                (if current = key
-//                                then attr.selected "selected"
-//                                else
-//                                    Attr.Empty)
-//                            ] [text value] :> Doc
-//                        )
-//                ) :> Doc
-//        ) detectChangeInOptionsAndItem
-//    let private SelectWithString' attrs (options: Var<Map<string,string>>) (targetLens:Var<string option>) (datatype: Var<'T option>)  =
-//        let detectChangeInOptionsAndItem = View.Map2 (fun opt target -> opt, target) options.View targetLens.View 
-//        Doc.BindView( fun ((map : Map<string,string>), (target: string option)) ->
-//            let mapList = Map.toList map
-//            let current =
-//                match target with
-//                | Some value -> value
-//                | None -> "-"
-//            selectAttr
-//                ([
-//                    attr.``class`` "fruit-form-select"
-//                    on.change (fun el _ -> targetLens.Set << Some <| getSelected el)
-//                    on.afterRender (fun el ->
-//                        try     
-//                            // if an item is loaded and there exists a list, but the value selected is not the same as the target, then select something in the list and update the item accordingly   
-//                            if selectedIndex el > -1 then
-//                                match (datatype.Value, getSelected el, List.tryHead mapList) with
-//                                | Some _, selectedValue, Some (key, _) when selectedValue <> current -> targetLens.Set <| Some key
-//                                | _ -> ()
-//                        with
-//                            | exn -> Console.Log exn
-//                        )
-//                    ] @ attrs )
-//                (
-//                    mapList
-//                    |> List.map ( fun (key, value) ->
-//                        Doc.Element
-//                            "option"
-//                            [
-//                                attr.``class`` "fruit-form-select-option"
-//                                attr.value key
-//                                (if current = key
-//                                then
-//                                    attr.selected "selected"
-//                                else
-//                                    Attr.Empty)
-//                            ] [text value] :> Doc
-//
-//                    )) :> Doc
-//        ) detectChangeInOptionsAndItem
-
